3e4fb09b0db794a1cc5e8380fbbeb1d6523dad00
Manuel Amesberger <manuel.amesberger@stud.th-deg.de>
2020-06-03 00:31:39 +0200

    added PCA
    Former-commit-id: 511f222c887d320604d20c2f78611f32004b7337

---
+++ PCA_on_image.py
@@ -0,0 +1,69 @@
+from sklearn.decomposition import PCA
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import matplotlib.pyplot as plt
+
+gen = ImageDataGenerator()
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   horizontal_flip = False)
+
+test_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   horizontal_flip = False)
+
+def train_images():
+    train_generator = train_datagen.flow_from_directory('dataset/training_set',
+                                                     target_size = (256, 256),
+                                                     color_mode='grayscale',
+                                                     batch_size = 400,
+                                                     shuffle= False
+                                                     )
+    x = train_generator
+    return x[0][0], x[0][1]
+
+
+def test_images():
+    test_generator = test_datagen.flow_from_directory('dataset/test_set',
+                                                     target_size = (256, 256),
+                                                     color_mode='grayscale',
+                                                     batch_size = 4,
+                                                     shuffle= False
+                                                     )
+    x = test_generator
+    return x[0][0], x[0][1]
+
+x_train, y_train = train_images()
+x_test, y_test = test_images()
+
+#apply some array magic to get the right shapes
+train_set = x_train.reshape(800, 65536)
+
+print (x_train.shape)
+print (x_test.shape)
+print (y_train.shape)
+print (y_test.shape)
+print (train_set.shape)
+print (train_set)
+
+
+x = train_set
+
+
+pca = PCA(.8)
+X_proj = pca.fit_transform(x)
+print (X_proj.shape)
+
+print (np.cumsum(pca.explained_variance_ratio_))
+
+fig = plt.figure(figsize=(8,8))
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+
+for i in range(10):
+    ax = fig.add_subplot(5, 5, i+1, xticks=[], yticks=[])
+    ax.imshow(np.reshape(pca.components_[i,:], (256,256)), cmap=plt.cm.bone, interpolation='nearest')
+
+plt.show()
+#x_train = pca.transform(x_train)
+#x_test = pca.transform(x_test)
