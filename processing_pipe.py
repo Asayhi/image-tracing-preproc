6c320e9639cbcec7d4b394b170a766b0f490bc94
Manuel Amesberger <manuel.amesberger@stud.th-deg.de>
2020-06-29 00:03:30 +0200

    unprocessed images are now saved individually
    Former-commit-id: ab9620d54cde92c474bd0f4b9a4eb9529d114322

--- processing_pipe.py
+++ processing_pipe.py
@@ -28,10 +28,11 @@ test_generator = test_datagen.flow_from_directory('dataset/test_set',
                                                     )
 images, y_images = next(test_generator)
 
-
+defOutputDir = "processing/Default/"
 acOutputDir = "processing/Autoencoder/"
 pcaOutputDir = "processing/PCA/"
 
+ac.ensureDirExists(defOutputDir)
 ac.ensureDirExists(acOutputDir)
 ac.ensureDirExists(pcaOutputDir)
 
@@ -39,19 +40,24 @@ ac.ensureDirExists(pcaOutputDir)
 fig = plt.figure(figsize=(3,3))
 fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
 
-for i in range(9):
+w = h = 3
+
+for i in range(image_count):
     image = images[i]
     image = image[:,:,0]
-    ax = fig.add_subplot(3, 3, i+1, xticks=[], yticks=[])
-    ax.imshow(image, cmap=plt.cm.bone, interpolation='nearest')
-
-plt.show()
+    fig = plt.figure(frameon=False) # Figure without frame
+    fig.set_size_inches(w,h)
+    ax = plt.Axes(fig, [0., 0., 1., 1.]) # Make the image fill out the entire figure
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    ax.imshow(image, cmap=plt.cm.bone, interpolation='nearest', aspect='auto')
+    fig.savefig(fname=defOutputDir + "Pic_"+'{0:03d}'.format(i))
 
 
 # autoencoder, modelDir = ac.loadAutoencoder()
 # prediction = autoencoder.predict(images, verbose=1)# you can now display an image to see it is reconstructed well
 # predictions = []
-w = h = 3
+
 
 # for i in range(image_count):
 #     fig = plt.figure(frameon=False) # Figure without frame
