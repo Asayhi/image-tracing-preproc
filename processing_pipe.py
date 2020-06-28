eed230b34f808c4a86851e2f0c0d0474e769d211
Manuel Amesberger <manuel.amesberger@stud.th-deg.de>
2020-06-28 23:49:40 +0200

    pca processed images are now saved individually
    Former-commit-id: a1997f429d953220f4e4a32b9c7c9f5b8a8eace4

--- processing_pipe.py
+++ processing_pipe.py
@@ -13,6 +13,8 @@ def loadPCA():
     pca_reload = pk.load(open(pca_path,'rb'))
     return pca_reload
 
+image_count = 9
+
 test_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,
                                    horizontal_flip = False)
@@ -21,15 +23,17 @@ test_datagen = ImageDataGenerator(rescale = 1./255,
 test_generator = test_datagen.flow_from_directory('dataset/test_set',
                                                     target_size = (256, 256),
                                                     color_mode='grayscale',
-                                                    batch_size = 9,
+                                                    batch_size = image_count,
                                                     shuffle= False
                                                     )
 images, y_images = next(test_generator)
 
 
 acOutputDir = "processing/Autoencoder/"
+pcaOutputDir = "processing/PCA/"
 
 ac.ensureDirExists(acOutputDir)
+ac.ensureDirExists(pcaOutputDir)
 
 
 fig = plt.figure(figsize=(3,3))
@@ -44,20 +48,36 @@ for i in range(9):
 plt.show()
 
 
-autoencoder, modelDir = ac.loadAutoencoder()
-prediction = autoencoder.predict(images, verbose=1)# you can now display an image to see it is reconstructed well
-predictions = []
+# autoencoder, modelDir = ac.loadAutoencoder()
+# prediction = autoencoder.predict(images, verbose=1)# you can now display an image to see it is reconstructed well
+# predictions = []
 w = h = 3
 
-for i in range(9):
+# for i in range(image_count):
+#     fig = plt.figure(frameon=False) # Figure without frame
+#     fig.set_size_inches(w,h)
+#     ax = plt.Axes(fig, [0., 0., 1., 1.]) # Make the image fill out the entire figure
+#     ax.set_axis_off()
+#     fig.add_axes(ax)
+#     x = prediction[i]
+#     ax.imshow(np.reshape(x, (256, 256)), cmap=plt.cm.bone, interpolation='nearest', aspect='auto')
+#     fig.savefig(fname=acOutputDir + "Pic_"+'{0:03d}'.format(i))
+
+
+pca = loadPCA()
+#apply array magic
+x = images.reshape(image_count, 65536)
+X_proj = pca.transform(x)
+
+x_inv_proj = pca.inverse_transform(X_proj)
+
+X_proj_img = x_inv_proj.reshape(image_count, 256, 256)
+
+for i in range(image_count):
     fig = plt.figure(frameon=False) # Figure without frame
     fig.set_size_inches(w,h)
     ax = plt.Axes(fig, [0., 0., 1., 1.]) # Make the image fill out the entire figure
     ax.set_axis_off()
     fig.add_axes(ax)
-    x = prediction[i]
-    ax.imshow(np.reshape(x, (256, 256)), cmap=plt.cm.bone, interpolation='nearest', aspect='auto')
-    fig.savefig(fname=acOutputDir + "Pic_"+'{0:03d}'.format(i))
-
-pca = loadPCA()
-X_proj = pca.fit_transform(images)
+    ax.imshow(X_proj_img[i], cmap=plt.cm.bone, interpolation='nearest', aspect='auto')
+    fig.savefig(fname=pcaOutputDir + "Pic_"+'{0:03d}'.format(i))
