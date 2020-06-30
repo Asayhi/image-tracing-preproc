88de2f9a7cc1b3f291b78b99edfde3ef6e984ea1
Manuel Amesberger <manuel.amesberger@stud.th-deg.de>
2020-06-30 02:33:09 +0200

    added Image Conversion to pipe
    Former-commit-id: 50fafe5eda4cd6df62c71be65682ae5831092fa9

--- processing_pipe.py
+++ processing_pipe.py
@@ -31,10 +31,15 @@ images, y_images = next(test_generator)
 defOutputDir = "processing/Default/"
 acOutputDir = "processing/Autoencoder/"
 pcaOutputDir = "processing/PCA/"
+vecDefDir = "processing/Vectorized/Default/"
+vecAcDir = "processing/Vectorized/Autoencoder/"
+vecPcaDir = "processing/Vectorized/PCA/"
+tempDir = "processing/tmp/"
 
-ac.ensureDirExists(defOutputDir)
-ac.ensureDirExists(acOutputDir)
-ac.ensureDirExists(pcaOutputDir)
+pathList = [defOutputDir, acOutputDir, pcaOutputDir, vecDefDir, vecAcDir, vecPcaDir, tempDir]
+
+for path in pathList:
+    ac.ensureDirExists(path)
 
 
 fig = plt.figure(figsize=(3,3))
@@ -42,6 +47,7 @@ fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
 
 w = h = 3
 
+#-----------------------------------Vanilla Images------------------------------------------
 for i in range(image_count):
     image = images[i]
     image = image[:,:,0]
@@ -54,22 +60,25 @@ for i in range(image_count):
     fig.savefig(fname=defOutputDir + "Pic_"+'{0:03d}'.format(i))
 
 
-# autoencoder, modelDir = ac.loadAutoencoder()
-# prediction = autoencoder.predict(images, verbose=1)# you can now display an image to see it is reconstructed well
-# predictions = []
+#-----------------------------------Autoencoder--------------------------------------------
+autoencoder, modelDir = ac.loadAutoencoder()
+prediction = autoencoder.predict(images, verbose=1)# you can now display an image to see it is reconstructed well
+predictions = []
 
 
-# for i in range(image_count):
-#     fig = plt.figure(frameon=False) # Figure without frame
-#     fig.set_size_inches(w,h)
-#     ax = plt.Axes(fig, [0., 0., 1., 1.]) # Make the image fill out the entire figure
-#     ax.set_axis_off()
-#     fig.add_axes(ax)
-#     x = prediction[i]
-#     ax.imshow(np.reshape(x, (256, 256)), cmap=plt.cm.bone, interpolation='nearest', aspect='auto')
-#     fig.savefig(fname=acOutputDir + "Pic_"+'{0:03d}'.format(i))
+for i in range(image_count):
+    fig = plt.figure(frameon=False) # Figure without frame
+    fig.set_size_inches(w,h)
+    ax = plt.Axes(fig, [0., 0., 1., 1.]) # Make the image fill out the entire figure
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    x = prediction[i]
+    ax.imshow(np.reshape(x, (256, 256)), cmap=plt.cm.bone, interpolation='nearest', aspect='auto')
+    fig.savefig(fname=acOutputDir + "Pic_"+'{0:03d}'.format(i))
 
 
+
+#------------------------------------PCA--------------------------------------------------
 pca = loadPCA()
 #apply array magic
 x = images.reshape(image_count, 65536)
@@ -87,3 +96,19 @@ for i in range(image_count):
     fig.add_axes(ax)
     ax.imshow(X_proj_img[i], cmap=plt.cm.bone, interpolation='nearest', aspect='auto')
     fig.savefig(fname=pcaOutputDir + "Pic_"+'{0:03d}'.format(i))
+
+
+#---------------------------------Image Conversion------------------------------------------
+for i in range(image_count):
+
+    #default
+    os.system("convert " + defOutputDir + "Pic_"+'{0:03d}'.format(i)+ ".png " + tempDir + "default.ppm")
+    os.system("potrace " + tempDir + "default.ppm --output " + vecDefDir + "potrace_Pic_"+'{0:03d}'.format(i) + ".svg -s")
+
+    #autoencoder
+    os.system("convert " + acOutputDir + "Pic_"+'{0:03d}'.format(i)+ ".png " + tempDir + "autoenc.ppm")
+    os.system("potrace " + tempDir + "autoenc.ppm --output " + vecAcDir + "potrace_Pic_"+'{0:03d}'.format(i) + ".svg -s")
+    
+    #pca
+    os.system("convert " + pcaOutputDir + "Pic_"+'{0:03d}'.format(i)+ ".png " + tempDir + "pca.ppm")
+    os.system("potrace " + tempDir + "pca.ppm --output " + vecPcaDir + "potrace_Pic_"+'{0:03d}'.format(i) + ".svg -s")
